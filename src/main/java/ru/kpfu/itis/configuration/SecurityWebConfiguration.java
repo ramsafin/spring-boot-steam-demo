91f3bd569ba9eb28c3153aa1225de78da8e6cd7a
Ramil Safin <RamilSafNab1996@gmail.com>
2016-11-04 15:12:18 +0300

    checkpoint

---
+++ src/main/java/ru/kpfu/itis/configuration/SecurityWebConfiguration.java
@@ -0,0 +1,11 @@
+package ru.kpfu.itis.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityWebConfiguration extends WebSecurityConfigurerAdapter {
+
+    // TODO oauth2 Steam configuration, bullshit
+
+}
---
+++ src/main/java/ru/kpfu/itis/controller/WelcomeController.java
@@ -0,0 +1,9 @@
+package ru.kpfu.itis.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+}
